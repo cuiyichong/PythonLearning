09ebe03a09bb69d34ac4834cbc26a3febc43bbf6
Yichong Cui <CYC@Yichongs-MacBook-Air.local>
2018-07-21 22:14:46 +0800

    python socket intro

---
+++ SocketLearn.py
@@ -0,0 +1,37 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print(s)
+
+server = 'pythonprogramming.net'
+port = 80
+
+server_ip = socket.gethostbyname(server)
+
+'''
+print(server_ip)
+
+request = "GET ? HTTP/1.1\nHost: "+server+"\n\n"
+s.connect((server.port))
+s.send(request.encode())
+result =s.recv(4096)
+
+print(result)
+
+while(len(result)>0):
+    print(result)
+    result.s.recv(1024)
+'''
+
+def pscan(port):
+    try:
+        s.connect((server.port))
+        return True
+    except:
+        return False
+
+for x in range(1,3000):
+    if pscan(x):
+        print('Port', x, 'is open!\n')
+  #  else:
+     #   print('Port', x, 'is closed')
